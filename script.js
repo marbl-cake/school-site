25b41b26e3883188bd6f81ff22fc91c089d312f5
marbl-cake <verificaditedesco@gmail.com>
2025-11-20 10:04:36 +0100

    psps

---
+++ script.js
@@ -0,0 +1,9 @@
+function search(){
+    const btn = document.getElementById('search-btn');
+    const bar = document.getElementById('search-bar');
+
+    bar.classList.toggle('show');  // Mostra/nasconde la barra
+    if (bar.classList.contains('show')) {
+      bar.focus();  // mette il cursore nella barra
+    }
+}
