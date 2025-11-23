5c81240ced40a9a19af9292bb2e0168693c4b708
marbl-cake <verificaditedesco@gmail.com>
2025-11-23 23:17:13 +0100

    carosel and small changes

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function search(){
 function toggleMenu() {
   document.getElementById("menu").classList.toggle("active");
 }
+
+let slideIndex = 0;
+
+function moveSlide(step) {
+  const slides = document.querySelectorAll('.carousel-images img');
+  slideIndex += step;
+  
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  } else if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  const newTransformValue = -slideIndex * 100 + '%';
+  document.querySelector('.carousel-images').style.transform = `translateX(${newTransformValue})`;
+}
+
