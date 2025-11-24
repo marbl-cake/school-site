4d1477c6c5b06f41d7674a6355eca1e87087d1da
marbl-cake <verificaditedesco@gmail.com>
2025-11-24 10:43:55 +0100

    indentation

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ function search(){
 }
 
 function toggleMenu() {
-  document.getElementById("menu").classList.toggle("active");
+    document.getElementById("menu").classList.toggle("active");
 }
 
 let slideIndex = 0;
 
 function moveSlide(step) {
-  const slides = document.querySelectorAll('.carousel-images img');
-  slideIndex += step;
-  
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  } else if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
+    const slides = document.querySelectorAll('.carousel-images img');
+    slideIndex += step;
 
-  const newTransformValue = -slideIndex * 100 + '%';
-  document.querySelector('.carousel-images').style.transform = `translateX(${newTransformValue})`;
+    if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+    } else if (slideIndex >= slides.length) {
+    slideIndex = 0;
+    }
+
+    const newTransformValue = -slideIndex * 100 + '%';
+    document.querySelector('.carousel-images').style.transform = `translateX(${newTransformValue})`;
 }
 
